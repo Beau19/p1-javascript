2a4575b37cced6d8ad3d32c31754c1bfda816e3a
Beau19 <9019316@student.zadkine.nl>
2022-10-06 13:37:38 +0200

    opzet opdracht 6.3

---
+++ opdracht_6.3/app.js
@@ -0,0 +1,11 @@
+function kleur_aanpassen() {
+    var aardappel = document.getElementById("title");
+    title.style.color = "red";
+    title.innerHTML = "Inhoud aangepast!";
+}
+
+function kleur_aanpassen2() {
+    var aardappel = document.getElementById("title");
+    title.style.color = "blue";
+    title.innerHTML = "Inhoud aangepast!";
+}
