4f7af9f1ed2906afc9e645f49aad36faeea665a0
Beau19 <9019316@student.zadkine.nl>
2022-10-07 09:41:20 +0200

    opzet opdracht 6.4

---
+++ opdracht_6.4/app.js
@@ -0,0 +1,22 @@
+function calculate(x, y) {
+  let answer = 1 + 12;
+  console.log(13)
+}
+
+calculate(1,12);
+antwoord: 13;
+
+function multiplyByFive(num){
+    let answer = num * 5;
+    console.log(answer);
+}
+
+multiplyByFive(7);
+
+function calculateMinutes(num) {
+   let answer = num / 60;
+   console.log(answer);
+}
+
+calculateMinutes(3600);
+antwoord: 60;
