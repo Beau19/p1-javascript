9b39cacd3d54f9d0b353e8b7eb14e25b8b0bb8cc
Beau19 <9019316@student.zadkine.nl>
2022-11-08 16:23:22 +0100

    javascript 8.1

--- opdracht_8.1/app.js
+++ opdracht_8.1/app.js
@@ -3,5 +3,3 @@ let multiply = (x, y, out) => out.innerText = x * y;
 let subtract = (x, y, out) => out.innerText = x - y;
 let add = (x, y, out) => out.innerText = x + y;
 let divide = (x, y, out) => out.innerText = x / y;
-
-// Ez peezy lemon fucking squeezy, vijf lijnen
